ed1190b20aeb5a2f11a02928ac47095c6629533d
paulbardar <q.c.tulip@gmail.com>
2017-08-01 13:24:36 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+;(function($){
+
+	$(window).load(function(){
+
+		var acc = document.getElementsByClassName("ba-accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+			acc[i].onclick = function(){
+				this.classList.toggle("active");
+
+				this.nextElementSibling.classList.toggle("show");
+			}
+		}
+
+	})
+
+	 var body = document.body,
+        btn = body.querySelector('.ba-toggle'),
+        overlay = body.querySelector('.ba-overlay');
+
+    btn.onclick = function(){
+    	body.classList.toggle('ba-menu-open');
+    }
+    overlay.onclick = function(){
+    	body.classList.toggle('ba-menu-open');
+    }
+
+
+})(jQuery);
